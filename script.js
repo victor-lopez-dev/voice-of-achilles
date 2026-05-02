59a6f922fe53535ccce88a0e2ba5e0fec8cb9e30
Victor Lopez <contact@victorlopez.me>
2026-05-02 00:12:03 -0400

    added nav modal and links

---
+++ script.js
@@ -0,0 +1,27 @@
+let navBtn = document.getElementById("hamburger-btn");
+let navBtnClose = document.getElementById("close-btn");
+let navMenu = document.querySelector(".modal");
+
+let navLinks = document.querySelectorAll("modal-link");
+let body1 = document.body;
+
+navBtn.addEventListener("click", function() {
+  navMenu.classList.toggle("display-modal");
+  navBtnClose.classList.toggle("display-modal");
+  body1.classList.toggle("display-modal");
+});
+
+navBtnClose.addEventListener("click", function() {
+  navMenu.classList.toggle("display-modal");
+  navBtnClose.classList.toggle("display-modal");
+  body1.classList.toggle("display-modal");
+});
+
+
+document.querySelectorAll('.modal-link').forEach(link => {
+  link.addEventListener('click', () => {
+    navMenu.classList.toggle('display-modal');
+    body1.classList.toggle("display-modal");
+    navBtnClose.classList.toggle("display-modal");
+  });
+});
