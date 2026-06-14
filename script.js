a0d96840d556e80a5e9a970b7b5425349e683e43
Victor Lopez <contact@victorlopez.me>
2026-06-14 00:23:42 -0400

    fixed modal problems

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
 let navBtn = document.getElementById("nav-btn");
-let navBtnClose = document.getElementById("close-btn");
 let navMenu = document.querySelector(".nav-modal");
+let logo = document.getElementById("logo");
 
-let navLinks = document.querySelectorAll("modal-link");
+let navLinks = document.querySelectorAll(".modal-link");
 let body1 = document.body;
 
 navBtn.addEventListener("click", function() {
   navMenu.classList.toggle("display-modal");
-  navBtnClose.classList.toggle("display-modal");
   body1.classList.toggle("display-modal");
 });
 
-navBtnClose.addEventListener("click", function() {
-  navMenu.classList.toggle("display-modal");
-  navBtnClose.classList.toggle("display-modal");
-  body1.classList.toggle("display-modal");
+logo.addEventListener("click", function() {
+  navMenu.classList.remove("display-modal");
+  body1.classList.remove("display-modal");
 });
-
 
 document.querySelectorAll('.modal-link').forEach(link => {
   link.addEventListener('click', () => {
-    navMenu.classList.toggle('display-modal');
-    body1.classList.toggle("display-modal");
-    navBtnClose.classList.toggle("display-modal");
+    navMenu.classList.remove('display-modal');
+    body1.classList.remove("display-modal");
   });
 });
